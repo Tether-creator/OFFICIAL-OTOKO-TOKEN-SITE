1fe2ae5cf03266d258c6aaddd209a960146c96d0
Tether-creator <gruthzillions@gmail.com>
2025-03-19 12:40:34 +0100

    Delete js directory

--- js/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("https://api.coingecko.com/api/v3/simple/price?ids=otokotoken&vs_currencies=usd")
-        .then(response => response.json())
-        .then(data => {
-            document.getElementById("token-price").innerText = "$" + data.otokotoken.usd;
-        })
-        .catch(error => console.error("Error fetching price:", error));
-});
